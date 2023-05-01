0a7d81e7599b90ca39b867e9751193b1fe6abba2
AlexCodreanu23 <126508471+AlexCodreanu23@users.noreply.github.com>
2023-05-01 14:44:30 +0300

    Create grammar.py

---
+++ grammar.py
@@ -0,0 +1,37 @@
+def verify(word, symbol):
+    if symbol not in grammar:
+        return False
+    elif not word and 'l' in grammar[symbol]:
+        return True
+    elif not word and symbol == 'S':
+        return True
+    else:
+        for i in grammar[symbol]:
+            if isinstance(i, tuple):
+                if word.startswith(i[0]) and i[0].islower():
+                    if verify(word[1:], i[1]):
+                        return True
+                elif word.startswith(i[0]) and i[0].isupper():
+                    if verify(i[1] + word, i[0]):
+                        return True
+            elif i == ('l',):
+                if verify(word, symbol):
+                    return True
+    return False
+
+
+
+
+grammar = {
+    'S': [('a', 'A'), ('d', 'E')],
+    'A': [('a', 'B'), ('a', 'S')],
+    'B': [('b', 'C')],
+    'C': [('b', 'D'), ('b', 'B')],
+    'D': [('c', 'D'), 'l'],
+    'E': ['l']
+}
+
+word=input("word= ")
+symbol='S'
+
+print(verify(word,symbol))
